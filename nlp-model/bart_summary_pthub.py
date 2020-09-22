0d731df4ba34de42ca1652f21f4e36bb5f206474
Sagar Patil <sagarreddypatil@gmail.com>
2020-09-22 15:51:33 -0400

    Ok so pthub was useless

--- nlp-model/bart_summary_pthub.py
+++ nlp-model/bart_summary_pthub.py
@@ -2,12 +2,14 @@ import torch
 import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 model_load_start = time.time()
 
 bart = torch.hub.load("pytorch/fairseq", "bart.large.cnn")
-bart.half()
-# bart.to(device)
+if device.type == "cuda":
+    bart.half()
+bart.to(device)
 bart.eval()
 
 print(f"Model loaded in {round(time.time() - model_load_start, 2)}s")
@@ -20,7 +22,12 @@ with open("sample.txt") as source:
 
 summarize_start = time.time()
 summary = bart.sample(
-    [sample_text], beam=3, lenpen=1.0, max_len_b=100, min_len=25, no_repeat_ngram_size=3
+    [sample_text],
+    beam=3,
+    lenpen=1.0,
+    max_len_b=100,
+    min_len=25,
+    no_repeat_ngram_size=3,
 )[0]
 
 reduction = 1 - len(summary) / len(sample_text)
